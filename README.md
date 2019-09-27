5218271b333a7d5709fb3df1ca45a77142548a4b
xju <xju@cern.ch>
2019-09-27 16:55:41 -0700

    add instructions to install

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # root_gnn
 GNN classification for reconstructed HEP events stored in ROOT
+
+## Install
+```bash
+conda create --name gnnana python=3.7
+conda install -c conda-forge jupyterlab
+conda install -c conda-forge root
+
+git clone https://github.com/xju2/root_gnn.git
+
+cd root_gnn
+pip install -e .
+```
--- root_gnn/prepare.py
+++ root_gnn/prepare.py
@@ -3,6 +3,15 @@ split one graph into input graph and output graph
 """
 import numpy as np
 
+def calc_dphi(phi1, phi2):
+    """Computes phi2-phi1 given in range [-pi,pi]"""
+    dphi = phi2 - phi1
+    if dphi > np.pi:
+        dphi -= 2*np.pi
+    if dphi < -np.pi:
+        dphi += 2*np.pi
+    return dphi
+
 def graph_to_input_target(graph):
     def create_feature(attr, fields):
         return np.hstack([np.array(attr[field], dtype=float) for field in fields])
