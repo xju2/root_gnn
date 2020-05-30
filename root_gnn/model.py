bad8ebcb9edf4171bd97dc24ec12383d4d6ea456
xju <xju@cern.ch>
2020-05-30 13:59:35 -0700

    add a classifier that simulateneously learns global and edge properties

--- root_gnn/model.py
+++ root_gnn/model.py
@@ -168,6 +168,40 @@ class GlobalClassifierNoEdgeInfo(snt.Module):
 
         return output_ops
 
+class EdgeGlobalClassifier(snt.Module):
+    def __init__(self, name="EdgeGlobalClassifier"):
+        super(GeneralClassifier, self).__init__(name=name)
+
+        self._encoder = MLPGraphIndependent()
+        self._core    = MLPGraphNetwork()
+        self._decoder = MLPGraphIndependent()
+
+        # Transforms the outputs into appropriate shapes
+        # assuming the target of the outputs of
+        # global and edge is binary.
+        global_output_size = 1
+        global_fn =lambda: snt.Sequential([
+            snt.nets.MLP([LATENT_SIZE, global_output_size],
+                         name='global_output'), tf.sigmoid])
+        edge_output_size = 1
+        edge_fn = lambda: snt.Sequential([
+            snt.nets.MLP([LATENT_SIZE, edge_output_size],
+                         name='edge_output'), tf.sigmoid])
+
+        self._output_transform = modules.GraphIndependent(edge_fn, None, global_fn)
+
+    def __call__(self, input_op, num_processing_steps):
+        latent = self._encoder(input_op)
+        latent0 = latent
+
+        output_ops = []
+        for _ in range(num_processing_steps):
+            core_input = utils_tf.concat([latent0, latent], axis=1)
+            latent = self._core(core_input)
+            decoded_op = self._decoder(latent)
+            output_ops.append(self._output_transform(decoded_op))
+
+        return output_ops
 
 class MultiClassifier(snt.Module):
     def __init__(self, name="MultiClassifier"):
