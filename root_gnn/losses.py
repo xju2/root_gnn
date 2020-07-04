0359afbc26798a89a0f211a9eddcc235092eea79
xju <xju@cern.ch>
2020-07-04 14:25:39 -0700

    update the loss and configuration

--- root_gnn/losses.py
+++ root_gnn/losses.py
@@ -22,6 +22,46 @@ class NodeEdgeLoss:
         return tf.stack(loss_ops)
 
 
+class GlobalLoss:
+    def __init__(self, real_global_weight, fake_global_weight):
+        self.w_global_real = real_global_weight
+        self.w_global_fake = fake_global_weight
+
+    def __call__(self, target_op, output_ops):
+        global_weights = target_op.globals * self.w_global_real \
+            + (1 - target_op.globals) * self.w_global_fake
+        loss_ops = [
+            tf.compat.v1.losses.log_loss(target_op.globals, output_op.globals, weights=global_weights)
+            for output_op in output_ops
+        ]
+        return tf.stack(loss_ops)
+
+
+class EdgeGlobalLoss:
+    def __init__(self, real_edge_weight, fake_edge_weight,
+                real_global_weight, fake_global_weight):
+        self.w_edge_real = real_edge_weight
+        self.w_edge_fake = fake_edge_weight
+        self.w_global_real = real_global_weight
+        self.w_global_fake = fake_global_weight
+
+    def __call__(self, target_op, output_ops):
+        global_weights = target_op.globals * self.w_global_real \
+            + (1 - target_op.globals) * self.w_global_fake
+        edge_weights = target_op.edges * self.w_edge_real \
+            + (1 - target_op.edges) * self.w_edge_fake
+
+        loss_ops = [
+            tf.compat.v1.losses.log_loss(target_op.globals, output_op.globals, weights=global_weights)
+            for output_op in output_ops
+        ]
+        loss_ops += [
+            tf.compact.v1.losses.log_loss(target_op.edges, output_op.edges, weights=edge_weights)
+            for output_op in output_ops
+        ]
+        return tf.stack(loss_ops)
+
+
 if __name__ == "__main__":
     node_edge_loss = NodeEdgeLoss(2, 1)
     node_edge_loss(1, 1)
