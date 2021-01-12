7277d67aa4cf87f62619d2383b209f45ebe219ff
Xiangyang Ju <xiangyang.ju@gmail.com>
2021-01-12 11:32:50 -0800

    move to a folder

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "graph_nets >= 1.1",
+        "graph_nets>=1.1",
         "future",
         "networkx",
         "numpy",
