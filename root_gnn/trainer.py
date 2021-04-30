b49b33e7c78f726c5941570df0477987886c7e49
dinganthony <anthonyding@berkeley.edu>
2021-04-30 13:17:57 -0700

    Documentation and tutorial for TrainerBase

--- root_gnn/trainer.py
+++ root_gnn/trainer.py
@@ -42,7 +42,6 @@ def read_dataset(filenames):
     """
     Read dataset...
     """
-    #AUTO = tf.data.experimental.AUTOTUNE
     tr_filenames = tf.io.gfile.glob(filenames)
     n_files = len(tr_filenames)
 
@@ -106,31 +105,50 @@ def get_signature(
         
     return input_signature
 
-#TODO: be flexible so that user can provide model/loss after initializing
-#e.g. trainer_train()
-
 class TrainerBase(object):
+    
+    """
+    The base class to implement a simple trainer and model.
+    
+    ...
+    
+    Important Attributes
+    --------------------
+    input_dir: The input directory for data
+    
+    output_dir: The output directory for metrics and outputs
+   
+    model: The model class to use for training, validating, and
+    testing
+    
+    loss_fcn: The loss function class to use for training,
+    validating, and testing
+    """
 
-    def __init__(self, train_dir=None, val_dir=None, output_dir=None, 
+    def __init__(self, input_dir=None, output_dir=None, 
                 model=None, loss_fcn=None, loss_name="GlobalLoss", lr=0.0005,
                 optimizer=None,
                 batch_size=50, num_epochs=5, num_iters=10,
                 metric_mode=None,
                 early_stop=None, max_attempts=1,
                 shuffle_size=1,
-                decay_lr=True, # if to use decay learning rate...
-                decay_lr_start_epoch=10,
                 patterns='*', distributed=False, verbose="INFO", 
                 **extra_configs):
-        self.train_dir = train_dir
-        self.val_dir = val_dir
+        """
+        TrainerBase constructor, which initializes configurations, hyperparameters,
+        and metrics.
+        
+        Parameters
+        ----------
+        Sets input, output, model and loss, as well as relevant hyperparameters.
+        The user can directly unpack the arguments from the ArgumentParser
+        created in get_arg_parser().
+        """
+        self.input_dir = input_dir
+        self.train_dir = os.path.join(self.input_dir, "train_*.tfrec")
+        self.val_dir = os.path.join(self.input_dir, "val_*.tfrec")
         self.output_dir = output_dir
         self.shuffle_size = shuffle_size
-
-        # model, loss_fcn, i/o dir are esseentials
-        # most others are hyperparams
-        # TODO 
-        self.hyperparams = {}
 
         self.num_iters = num_iters
         self.batch_size = batch_size
@@ -141,7 +159,6 @@ class TrainerBase(object):
         self.model = model
         self.loss_fcn = loss_fcn
         self.loss_name = loss_name
-        # snt optimizer
         if optimizer:
             self.optimizer = optimizer(learning_rate=self.lr)
         else:
@@ -149,9 +166,6 @@ class TrainerBase(object):
         
         self.init_metrics(metric_mode, early_stop, max_attempts)
 
-        # self.decay_lr_start_epoch = tf.constant(decay_lr_start_epoch, dtype=tf.int32)
-        # self.decay_lr = decay_lr # if use de
-        # perform distributed training
         self.distributed = distributed
         self.extra_configs = extra_configs
 
@@ -159,13 +173,14 @@ class TrainerBase(object):
     # --------------------
     @staticmethod
     def get_arg_parser():
+        """
+        This static method returns an ArgumentParser with important arguments for
+        the TrainerBase's configuration. The parser can be used as a parent parser.
+        """
         parser = argparse.ArgumentParser(add_help=False)
         add_arg = parser.add_argument
-        add_arg("--train-dir", help="name of dir for training", default="tfrec/train_*.tfrec") # TODO: required
-        add_arg("--val-dir", help="name of dir for validation", default="tfrec/val_*.tfrec") # TODO: required
-        #TODO: have one input directory: train/val/testing
-        add_arg("--output-dir", help="name of dir for output", default="trained") # TODO: required, everything else can be underhyperparams
-        add_arg("--prod-name", help="inner dir for output", default="noedge_fullevts") 
+        add_arg("--input-dir", help="name of dir for input", default="tfrec")
+        add_arg("--output-dir", help="name of dir for output", default="trained")
         add_arg("--batch-size", type=int, help="training/evaluation batch size", default=50)
         add_arg("--num-epochs", type=int, help="number of epochs", default=5)
         add_arg("--num-iters", type=int, help="level of message passing", default=10)
@@ -173,6 +188,26 @@ class TrainerBase(object):
         return parser
 
     def init_metrics(self, mode=None, early_stop=None, max_attempts=1):
+        """
+        Function to initialize TrainerBase metrics, which is called in
+        __init__().
+        
+        Parameters
+        ----------
+        mode: Either 'clf' (classification) or 'rgr' (regression).
+        Determines which metrics will be used:
+            - 'clf' uses 'auc', 'acc', 'pre', 'rec', 'loss'
+            - 'rgr' uses 'loss', 'pull'
+        
+        early_stop: Sets a metric to use as a stopping condition
+        
+        max_attempts: The number of attempts allowed for the early_stop
+        metric to fail before training stops
+        
+        Raises
+        ------
+        ValueError: if self.metric_mode is neither 'clf' or 'rgr'
+        """
         self.metric_mode = mode
         if mode is None:
             self.metric_dict = {}
@@ -200,7 +235,12 @@ class TrainerBase(object):
         time_stamp = time.strftime('%Y%m%d-%H%M%S', time.localtime())
         self.metric_writer = tf.summary.create_file_writer(os.path.join(self.output_dir, "logs/{}/metrics".format(time_stamp)))
     
-    def early_stop_condition(self):
+    def _early_stop_condition(self):
+        """
+        Helper function to check whether, given self.early_stop
+        and self.should_max_metric, training should stop at the
+        current moment.
+        """
         current_metric = self.metric_dict[self.early_stop]
         if (self.should_max_metric and current_metric > self.best_metric) or\
                 (not self.should_max_metric and current_metric < self.best_metric):
@@ -214,53 +254,111 @@ class TrainerBase(object):
             self.best_metric = current_metric
         return False
 
-    def get_shuffle_size(self, shuffle_size):
-        # default behavior for train_classifier.py, can override for other models
+    def _get_shuffle_size(self, shuffle_size):
+        """
+        Helper function that returns the buffer size to shuffle with.
+        """
         return self.ngraphs_train if shuffle_size < 0 else shuffle_size
 
-    def load_training_data(self, filenames=None, shuffle=False, shuffle_size=None, repeat=1):
+    def load_training_data(self, filenames=None, shuffle=False, shuffle_size=-1, repeat=1):
+        """
+        Loads, shuffles, and sets up training data from the train directory.
+        
+        Parameters
+        ----------
+        filenames: A list of pattern-matching files, produced by tf.io.gfile.glob.
+        Defaults to None, in which case self.train_dir is used for glob
+        
+        shuffle: A boolean indicating whether to shuffle the dataset or not
+        
+        shuffle_size: If shuffle is True, specifies the shuffle buffer size. If
+        negative, uses the entire dataset as is
+        
+        repeat: Specifies how many times the dataset is repeated
+        """
         if not filenames:
             filenames = tf.io.gfile.glob(self.train_dir)
         if shuffle_size is None:
             shuffle_size = self.shuffle_size
         self.data_train, self.ngraphs_train = read_dataset(filenames)
         if shuffle:
-            self.data_train = self.data_train.shuffle(self.get_shuffle_size(shuffle_size), seed=12345, reshuffle_each_iteration=False)
+            self.data_train = self.data_train.shuffle(self._get_shuffle_size(shuffle_size), seed=12345, reshuffle_each_iteration=False)
         if repeat > 1:
             self.data_train = self.data_train.repeat(repeat)
         self.data_train = self.data_train.prefetch(AUTO)
         return self.data_train, self.ngraphs_train
 
-    def load_validating_data(self, filenames=None, shuffle=False, shuffle_size=None, repeat=1):
+    def load_validating_data(self, filenames=None, shuffle=False, shuffle_size=-1, repeat=1):
+        """
+        Loads, shuffles, and sets up validation data from the val directory.
+        
+        Parameters
+        ----------
+        filenames: A list of pattern-matching files, produced by tf.io.gfile.glob.
+        Defaults to None, in which case self.val_dir is used for glob
+        
+        shuffle: A boolean indicating whether to shuffle the dataset or not
+        
+        shuffle_size: If shuffle is True, specifies the shuffle buffer size. If
+        negative, uses the entire dataset as is
+        
+        repeat: Specifies how many times the dataset is repeated
+        """
         if not filenames:
             filenames = tf.io.gfile.glob(self.val_dir)
         if shuffle_size is None:
             shuffle_size = self.shuffle_size
         self.data_val, self.ngraphs_val = read_dataset(filenames)
         if shuffle:
-            self.data_val = self.data_val.shuffle(self.get_shuffle_size(shuffle_size), seed=12345, reshuffle_each_iteration=False)
+            self.data_val = self.data_val.shuffle(self._get_shuffle_size(shuffle_size), seed=12345, reshuffle_each_iteration=False)
         if repeat > 1:
             self.data_val = self.data_val.repeat(repeat)
         self.data_val = self.data_val.prefetch(AUTO)
         return self.data_val, self.ngraphs_val
 
-    def load_testing_data(self, filenames, shuffle=False, shuffle_size=None, repeat=1):
+    def load_testing_data(self, filenames, shuffle=False, shuffle_size=-1, repeat=1):
+        """
+        Loads, shuffles, and sets up testing data from the test directory.
+        
+        Parameters
+        ----------
+        filenames: A list of pattern-matching files, produced by tf.io.gfile.glob
+        
+        shuffle: A boolean indicating whether to shuffle the dataset or not
+        
+        shuffle_size: If shuffle is True, specifies the shuffle buffer size. If
+        negative, uses the entire dataset as is
+        
+        repeat: Specifies how many times the dataset is repeated
+        """
         self.data_test, self.ngraphs_test = read_dataset(filenames)
         if shuffle_size is None:
             shuffle_size = self.shuffle_size
         if shuffle:
-            self.data_test = self.data_test.shuffle(self.get_shuffle_size(shuffle_size), seed=12345, reshuffle_each_iteration=False)
+            self.data_test = self.data_test.shuffle(self._get_shuffle_size(shuffle_size), seed=12345, reshuffle_each_iteration=False)
         if repeat > 1:
             self.data_test = self.data_test.repeat(repeat)
         self.data_test = self.data_test.prefetch(AUTO)
         return self.data_test, self.ngraphs_test
 
     def setup_training_loop(self, model=None, loss_fcn=None):
+        """
+        Sets up training step for the optimizer using tf.function
+        and tf.GradientTape. Optionally, the user can pass in a
+        model and loss_fcn to replace the current model and loss_fcn
+        attributes of TrainerBase.
+        
+        Parameters
+        ----------
+        model: The model class to use for training
+        
+        loss_fcn: The loss function class to use for training
+        """
         if model:
             self.model = model
         if loss_fcn:
             self.loss_fcn = loss_fcn
-        input_signature = self.input_signature()
+        input_signature = self._input_signature()
 
         def update_step(inputs, targets):
             print("Tracing update_step")
@@ -275,22 +373,45 @@ class TrainerBase(object):
 
         self.training_step = tf.function(update_step, input_signature=input_signature)
 
-    def input_signature(self):
+    def _input_signature(self):
+        """
+        Helper function to generate an input signature, which is
+        passed into tf.function() to generate the training_step.
+        """
         return get_signature(self.data_train, self.batch_size)
 
     def optimizer(self, lr):
+        """
+        Sets the optimizer.
+        """
         self.optimizer = snt.optimizers.Adam(lr)
 
     def set_model(self, model):
+        """
+        Sets the model.
+        """
         self.model = model
 
     def set_loss(loss_fcn):
+        """
+        Sets the loss function.
+        """
         self.loss_fcn = loss_fcn
 
     # Training
     # --------------------        
 
     def train_one_epoch(self, train_data=None):
+        """
+        Performs one epoch of training, returning the training loss
+        and number of batches.
+        
+        Parameters
+        ----------
+        train_data: The data to use for training. Must have been
+        processed by load_training_data() beforehand. If None is
+        specified, defaults to self.data_train
+        """
         if train_data is None:
             train_data = self.data_train
         num_batches = 0
@@ -302,11 +423,22 @@ class TrainerBase(object):
         return total_loss, num_batches
 
     def validate_one_epoch(self, val_data=None):
+        """
+        Performs one epoch of validation, returning the validation loss,
+        number of batches, predictions, as well as truth info.
+        
+        Parameters
+        ----------
+        val_data: The data to use for validation. Must have been
+        processed by load_validating_data() beforehand. If None is
+        specified, defaults to self.data_val
+        """
         if val_data is None:
             val_data = self.data_val
         total_loss = 0.
         num_batches = 0
         predictions, truth_info = [], []
+
         for data in loop_dataset(val_data, self.batch_size):
             inputs, targets = data
             outputs = self.model(inputs, self.num_iters)
@@ -325,9 +457,26 @@ class TrainerBase(object):
         predictions = np.concatenate(predictions, axis=0)
         truth_info = np.concatenate(truth_info, axis=0)
         return total_loss, num_batches, predictions, truth_info
-
+    
     def update_metrics(self, predictions, truth_info, loss, threshold=0.5):
+        """
+        Updates metrics after receiving both predictions and truth_info from validation.
         
+        Parameters
+        ----------
+        predictions: Predictions generated from validation
+        
+        truth_info: Truth info used in validation
+        
+        loss: Validation loss
+        
+        Threshold: For the 'clf' metric mode (classification), indicates the threshold
+        for classifying on-class or off-class. Defaults to 0.5
+        
+        Raises
+        ------
+        ValueError: if self.metric_mode is neither 'clf' or 'rgr'
+        """
         if self.metric_mode == "clf":
             y_true, y_pred = (truth_info > threshold), (predictions > threshold)
             fpr, tpr, _ = sklearn.metrics.roc_curve(y_true, predictions)
@@ -347,9 +496,27 @@ class TrainerBase(object):
             self.metric_writer.flush()
         self.epoch_count.assign_add(1)
 
-    # Trains the dataset. If train_data and val_data are specified, it uses those as the dataset.
-    # Otherwise, it uses self.data_train and self.data_val attributes of the TrainerBase object.
     def train(self, model=None, loss_fcn=None, train_data=None, val_data=None, num_epochs=None):
+        """
+        Trains the dataset, potentially using the specified model and loss.
+        
+        Parameters
+        ----------
+        model: Class of model used for training. If not specified, uses current
+        model attribute of TrainerBase
+        
+        loss_fcn: Class of loss function used for training. If not specified,
+        uses current loss_fcn attribute of TrainerBase
+        
+        train_data: Data to train on. Defaults to self.data_train, which should
+        be loaded beforehand by load_training_data()
+        
+        val_data: Data to use for validation. Defaults to self.data_val, which
+        should be loaded beforehand by load_validating_data()
+        
+        num_epochs: The maximum number of epochs for training. Defaults to 
+        self.num_epochs
+        """
         if num_epochs is None:
             num_epochs = self.num_epochs
         self.setup_training_loop(model, loss_fcn)
@@ -358,18 +525,31 @@ class TrainerBase(object):
             loss_val, num_batches_val, predictions, truth_info = self.validate_one_epoch(val_data)
             if self.metric_mode:
                 self.update_metrics(predictions, truth_info, loss_val / num_batches_val)
-            if self.early_stop_condition():
-                print("breaking after {} epochs".format(epochs))
+            if self._early_stop_condition():
+                print("breaking after {} epoch(s)".format(epoch + 1))
                 break
 
     # Prediction
     # --------------------  
 
     def predict(self, test_data):
+        """
+        Uses the current model/loss to generate predictions on test_data
+        """
         raise NotImplementedError
 
-    def score(self):
+    def score(self, y_pred, y_true):
+        """
+        Returns a dictionary of relevant metrics.
+        
+        Parameters
+        ----------
+        y_pred: Values predicted by the model
+        
+        y_true: The ground truth values to score against
+        """
         raise NotImplementedError
 
-    def eval(self, model):
-        raise NotImplementedError
+    # future: add hyperparameter optimization
+    # search for tf hyperparam optimization packages, integrate with tensorboard
+    # can try multiple hyperparams, construct surrogate fn predicting hyperparam relationships
