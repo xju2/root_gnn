5a997d6c784ab5df6bac4851ea25acabd071373e
Landon Reed <reed0658@d.umn.edu>
2021-06-24 13:35:09 -0700

    Flipped back the loop order in the read function and added a threshhold for dR

--- root_gnn/src/datasets/tauid.py
+++ root_gnn/src/datasets/tauid.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import itertools
 
 from graph_nets import utils_tf
@@ -8,8 +9,6 @@ from root_gnn.src.datasets.base import DataSet
 def make_graph(event, debug=False):
     # n_max_nodes = 60
     n_nodes = event.n_nodes
-    #nodes = np.hstack((event.jet_pt, event.jet_eta, event.jet_phi, event.jet_e))
-    #nodes = nodes.reshape( (n_nodes, 4) )
     nodes = np.array(event.nodes)
     if debug:
         #print(np.array(event.jet_pt).shape)
@@ -48,7 +47,7 @@ def make_graph(event, debug=False):
     return [(input_graph, target_graph)]
 
 class JetInfo:
-    def __init__(self, is_tau_jet: bool, nodes, n_nodes: int):
+    def __init__(self, is_tau_jet: bool, nodes: list[list[float]], n_nodes: int):
         self.is_tau_jet = is_tau_jet
         self.n_nodes = n_nodes
         self.nodes = nodes
@@ -61,7 +60,7 @@ def read(filename):
     chain = TChain(tree_name,tree_name)
     chain.Add(filename)
     n_entries = chain.GetEntries()
-    
+
     JetPhi = std.vector('float')()
     JetEta = std.vector('float')()
 
@@ -103,93 +102,42 @@ def read(filename):
 
     chain.SetBranchAddress("JetGhostTrackN",JetGhostTrackN)
     chain.SetBranchAddress("JetGhostTrackIdx",JetGhostTrackIdx)
-    print("Entries",n_entries,len(nJets),len(nTruthJets))
 
+    isTau = 0
     for ientry in range(n_entries):
         chain.GetEntry(ientry)
         track_idx = 0
         tower_idx = 0
-        print(nJets[0],nTruthJets[0],len(JetPhi),len(TruthJetPhi))
-
-        #################################################
-        # Match truth jets to the closest (in terms of angular distance)
-        # reconstructed jet while ensuring each reconstructed jet is
-        # matched only once
-        #################################################
-        used_buffer = [ False for x in range(nJets[0]) ]
-        # Initialize jet labels to mean false. Iterate over truth jets to find
-        # which ones need to be overriden with true labels
-        is_tau_jet = [ 0 for x in range(nJets[0]) ]
-        for itruth in range(nTruthJets[0]):
-            min_index = 0
-            if nJets[0] > 0:
-                min_dR2 = (JetPhi[0]-TruthJetPhi[itruth])**2 + (JetEta[0]-TruthJetEta[itruth])**2
-            for ijet in range(nJets[0]):
-                dR2 = (JetPhi[ijet]-TruthJetPhi[itruth])**2 + (JetEta[ijet]-TruthJetEta[itruth])**2
-                if dR2 < min_dR2 and not used_buffer[ijet]:
-                    min_dR2 = dR2
-                    min_index = ijet
-            # Change label if reconstructed jet with index min_index is unused
-            if nJets[0] > 0 and not used_buffer[min_index]:
-                is_tau_jet[min_index] = TruthJetIsTautagged[itruth]
-                used_buffer[min_index] = True
-        #################################################
-        # Retrieve the track and tower information for
-        # each reconstructed jet and yield the information
-        # needed to build the jet's graph representation
-        #################################################
         for ijet in range(nJets[0]):
+            # Match jet to truth jet that minimizes angular distance
             nodes = []
+            min_index = 0
+            if nTruthJets[0] > 0:
+                min_dR = math.sqrt((JetPhi[ijet]-TruthJetPhi[0])**2 + (JetEta[ijet]-TruthJetEta[0])**2)
+            for itruth in range(nTruthJets[0]):
+                dR = math.sqrt((JetPhi[ijet]-TruthJetPhi[itruth])**2 + (JetEta[ijet]-TruthJetEta[itruth])**2)
+                if dR < min_dR:
+                    min_dR = dR
+                    min_index = itruth
+            if nTruthJets[0] > 0 and min_dR < 0.4:
+                isTau = TruthJetIsTautagged[min_index]
+            else:
+                isTau = 0
+
             for itower in range(JetTowerN[ijet]):
                 nodes.append([JetTowerEt[tower_idx],JetTowerEta[tower_idx],JetTowerPhi[tower_idx]])
                 tower_idx += 1
-                #generate tower list
 
-            for itrack in range(JetGhostTrackN[ijet]): # towers associated with this jet
+            for itrack in range(JetGhostTrackN[ijet]):
                 ghost_track_idx = JetGhostTrackIdx[track_idx]
-                nodes.append([TrackPt[ghost_track_idx],TrackEta[ghost_track_idx],TrackPhi[ghost_track_idx]])
+               
                 track_idx+=1
+                nodes.append([TrackPt[ghost_track_idx],TrackEta[ghost_track_idx],TrackPhi[ghost_track_idx]])
 
-            yield JetInfo(is_tau_jet[ijet],nodes,len(nodes))
-
-        # Print debug info
-        #if nTruthJets[0] > 0 and nJets[0] > nTruthJets[0]:
-        if 1:
-            chain.Scan("nJets:nTruthJets:JetTowerN:JetTowerEt:JetTowerEta:JetTowerPhi:JetGhostTrackN:JetGhostTrackIdx:TrackPt:TrackEta:TrackPhi","","",50,ientry)
-            chain.Scan("nJets:nTruthJets:TruthJetIsTautagged:TruthJetEta:TruthJetPhi:JetEta:JetPhi:JetTowerN:JetGhostTrackN","","",50,ientry)
-            input()
+            yield JetInfo(isTau,nodes,len(nodes))
 
 class TauIdentificationDataSet(DataSet):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.read = read
         self.make_graph = make_graph
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import networkx as nx
-
-from graph_nets import utils_np
-from graph_nets import graphs
-
-t = TauIdentificationDataSet()
-
-def print_graphs_tuple(g, data=True):
-    for field_name in graphs.ALL_FIELDS:
-        per_replica_sample = getattr(g, field_name)
-        if per_replica_sample is None:
-            print(field_name, "EMPTY")
-        else:
-            print(field_name, "is with shape", per_replica_sample.shape)
-            if data and  field_name != "edges":
-                print(per_replica_sample)
-
-for y in t.read("~/tauid_gnn/root_gnn/root_gnn/src/datasets/v0/Ntuple_ditau_processed.root"):
-    gin, gtarget = make_graph(y,False)[0]
-    print("Input")
-    print_graphs_tuple(gin,data=True)
-    print("Target")
-    print_graphs_tuple(gtarget,data=True)
-    #Pause execution for user to read debug info
-    input()
