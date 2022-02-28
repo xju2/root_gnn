342c18bc46847ee9113fffb45acae82461b87cac
Landon Reed <reed0658@d.umn.edu>
2022-02-28 14:35:22 -0800

    Fixed some typos and added model that applies the edge block first

--- root_gnn/src/models/global_learner.py
+++ root_gnn/src/models/global_learner.py
@@ -6,7 +6,7 @@ from graph_nets import modules
 from graph_nets import blocks
 
 from root_gnn.src.models.base import MLPGraphNetwork
-from root_gnn.src.models.base import make_mlp_model, make_multi_mlp_model
+from root_gnn.src.models.base import make_mlp_model, make_multi_mlp_model, make_concat_mlp_model
 from functools import partial
 
 
@@ -27,7 +27,7 @@ class GlobalLearnerBase(snt.Module):
         if encoder_size is not None:
             encoder_mlp_fn = partial(encoder_fn, mlp_size=encoder_size, name="EncoderMLP", **kwargs)
         else:
-            encoder_mlp_fn = partial(ecoder_fn, name='EncoderMLP', **kwargs)
+            encoder_mlp_fn = partial(encoder_fn, name='EncoderMLP', **kwargs)
 
         node_block_args=dict(use_received_edges=False, use_sent_edges=False, use_nodes=True, use_globals=False,received_edges_reducer=reducer,sent_edges_reducer=reducer)
         edge_block_args=dict(use_edges=False, use_receiver_nodes=True, use_sender_nodes=True, use_globals=False)
@@ -335,7 +335,7 @@ class GlobalClassifierMultiMLP(GlobalLearnerBase):
 class GlobalClassifierConcatMLP(GlobalLearnerBase):
     def __init__(self,
         with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
-        encoder_size: list=None, core_size: list=None, decoder_size: list=None,encoder_fn=make_multi_mlp_model,
+        encoder_size: list=None, core_size: list=None, decoder_size: list=None,encoder_fn=make_concat_mlp_model,
         name="GlobalClassifier", **kwargs):
 
         global_output_size = 1
@@ -509,3 +509,101 @@ class GlobalAttentionClassifier(snt.Module):
             weighted_input, num_processing_steps, is_training=is_training)
         
         return output_graph
+
+    
+class GlobalLearnerBaseEdgesFirst(snt.Module):
+    def __init__(self,
+        global_fn,
+        with_edge_inputs=False,
+        with_node_inputs=True,
+        with_global_inputs=False,
+        encoder_size: list=None,
+        core_size: list=None,
+        name="GlobalLearnerBase",
+        reducer=tf.math.unsorted_segment_sum,
+        encoder_fn = make_mlp_model, **kwargs):
+        super(GlobalLearnerBase, self).__init__(name=name)
+
+        if encoder_size is not None:
+            encoder_mlp_fn = partial(encoder_fn, mlp_size=encoder_size, name="EncoderMLP", **kwargs)
+        else:
+            encoder_mlp_fn = partial(ecoder_fn, name='EncoderMLP', **kwargs)
+
+        node_block_args=dict(use_received_edges=True, use_sent_edges=True, use_nodes=True, use_globals=False,received_edges_reducer=reducer,sent_edges_reducer=reducer)
+        edge_block_args=dict(use_edges=False, use_receiver_nodes=True, use_sender_nodes=True, use_globals=False)
+        global_block_args=dict(use_edges=True, use_nodes=True, use_globals=False,nodes_reducer=reducer,edges_reducer=reducer)
+        print("Global Inputs?:",with_global_inputs)
+
+        if with_edge_inputs:
+            edge_block_args['use_edges'] = True
+            node_block_args['use_received_edges'] = False
+            node_block_args['use_sent_edges'] = False
+        if not with_node_inputs:
+            edge_block_args['use_receiver_nodes'] = False
+            edge_block_args['use_sender_nodes'] = False
+            node_block_args['use_nodes'] = False
+        if with_global_inputs:
+            global_block_args['use_globals'] = True
+            global_block_args['use_nodes'] = False
+            global_block_args['use_edges'] = False
+
+        self._edge_encoder_block = blocks.EdgeBlock(
+            edge_model_fn=encoder_mlp_fn,
+            name='edge_encoder_block', **edge_block_args)
+
+        self._node_encoder_block = blocks.NodeBlock(
+            node_model_fn=encoder_mlp_fn,
+            name='node_encoder_block', **node_block_args)
+
+        self._global_block = blocks.GlobalBlock(
+            global_model_fn=encoder_mlp_fn, name='global_encoder_block', **global_block_args)
+
+        if core_size is not None:
+            core_mlp_fn = partial(make_mlp_model, mlp_size=core_size, **kwargs)
+        else:
+            core_mlp_fn = partial(make_mlp_model, **kwargs)
+
+        self._core = MLPGraphNetwork(nn_fn=core_mlp_fn, reducer=reducer)
+
+        self._output_transform = modules.GraphIndependent(None, None, global_fn)
+
+    def __call__(self, input_op, num_processing_steps, is_training=True):
+        node_kwargs = edge_kwargs = global_kwargs = dict(is_training=is_training)
+
+        latent = self._global_block(
+            self._node_encoder_block(self._edge_encoder_block(input_op, node_kwargs), edge_kwargs), global_kwargs)
+        latent0 = latent
+
+        output_ops = []
+        for _ in range(num_processing_steps):
+            core_input = utils_tf.concat([latent0, latent], axis=1)
+            latent = self._core(core_input, edge_kwargs, node_kwargs, global_kwargs)
+            output_ops.append(self._output_transform(latent))
+
+        return output_ops
+
+
+class GlobalClassifierEdgesFirst(GlobalLearnerBase):
+    def __init__(self,
+        with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
+        encoder_size: list=None, core_size: list=None, decoder_size: list=None,
+        name="GlobalClassifier", **kwargs):
+
+        global_output_size = 1
+        if decoder_size is not None:
+            decoder_size += [global_output_size]
+        else:
+            decoder_size = [global_output_size]
+
+        global_fn =lambda: snt.Sequential([
+            snt.nets.MLP(decoder_size,
+                        activation=tf.nn.relu, # default is relu
+                        name='edge_classifier_output'),
+            tf.sigmoid])
+
+        super().__init__(global_fn,
+            with_edge_inputs=with_edge_inputs,
+            with_node_inputs=with_node_inputs,
+            with_global_inputs=with_global_inputs,
+            encoder_size=encoder_size, core_size=core_size,
+            name=name, **kwargs)
