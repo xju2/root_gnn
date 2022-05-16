e4c7f0f2dca7c7012db681c15ba9d90663d62e26
Landon Reed <reed0658@d.umn.edu>
2022-05-16 14:27:33 -0700

    Turn on the edge features in the models that support separate node/edge encoders

--- root_gnn/src/models/global_learner.py
+++ root_gnn/src/models/global_learner.py
@@ -33,7 +33,7 @@ class GlobalLearnerBase(snt.Module):
             edge_encoder_mlp_fn = partial(edge_encoder_fn, name='EdgeEncoderMLP', **kwargs)
 
         node_block_args=dict(use_received_edges=False, use_sent_edges=False, use_nodes=True, use_globals=False,received_edges_reducer=reducer,sent_edges_reducer=reducer)
-        edge_block_args=dict(use_edges=False, use_receiver_nodes=True, use_sender_nodes=True, use_globals=False)
+        edge_block_args=dict(use_edges=True, use_receiver_nodes=True, use_sender_nodes=True, use_globals=False)
         global_block_args=dict(use_edges=True, use_nodes=True, use_globals=False,nodes_reducer=reducer,edges_reducer=reducer)
         print("Global Inputs?:",with_global_inputs)
 
@@ -72,9 +72,12 @@ class GlobalLearnerBase(snt.Module):
 
     def __call__(self, input_op, num_processing_steps, is_training=True):
         node_kwargs = edge_kwargs = global_kwargs = dict(is_training=is_training)
+        
+        node_encoding = self._node_encoder_block(input_op, node_kwargs)
+        edge_encoding = self._edge_encoder_block(node_encoding, edge_kwargs)
 
-        latent = self._global_block(
-            self._edge_encoder_block(self._node_encoder_block(input_op, node_kwargs), edge_kwargs), global_kwargs)
+
+        latent = self._global_block(edge_encoding, global_kwargs)
         latent0 = latent
 
         output_ops = []
@@ -88,7 +91,7 @@ class GlobalLearnerBase(snt.Module):
 
 class GlobalClassifier(GlobalLearnerBase):
     def __init__(self,
-        with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
+        with_edge_inputs=True, with_node_inputs=True, with_global_inputs=False,
         encoder_size: list=None, core_size: list=None, decoder_size: list=None,
         name="GlobalClassifier", **kwargs):
 
@@ -110,158 +113,6 @@ class GlobalClassifier(GlobalLearnerBase):
             with_global_inputs=with_global_inputs,
             encoder_size=encoder_size, core_size=core_size,
             name=name, **kwargs)
-
-class GlobalClassifierNodeH(GlobalLearnerBase):
-    def __init__(self,
-        with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
-        encoder_size: list=None, core_size: list=None, decoder_size: list=None,
-        name="GlobalClassifier", **kwargs):
-
-        global_output_size = 1
-        if decoder_size is not None:
-            decoder_size += [global_output_size]
-        else:
-            decoder_size = [global_output_size]
-
-        global_fn =lambda: snt.Sequential([
-            snt.nets.MLP(decoder_size,
-                        activation=tf.nn.relu, # default is relu
-                        name='edge_classifier_output'),
-            tf.sigmoid])
-
-        super().__init__(global_fn,
-            with_edge_inputs=with_edge_inputs,
-            with_node_inputs=with_node_inputs,
-            with_global_inputs=with_global_inputs,
-            encoder_size=encoder_size, core_size=core_size,
-            encoder_fn = make_mlp_model,node_encoder_fn = make_multi_mlp_model,edge_encoder_fn = make_mlp_model,
-            name=name, **kwargs)
-        
-class GlobalClassifierEdgeH(GlobalLearnerBase):
-    def __init__(self,
-        with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
-        encoder_size: list=None, core_size: list=None, decoder_size: list=None,
-        name="GlobalClassifier", **kwargs):
-
-        global_output_size = 1
-        if decoder_size is not None:
-            decoder_size += [global_output_size]
-        else:
-            decoder_size = [global_output_size]
-
-        global_fn =lambda: snt.Sequential([
-            snt.nets.MLP(decoder_size,
-                        activation=tf.nn.relu, # default is relu
-                        name='edge_classifier_output'),
-            tf.sigmoid])
-
-        super().__init__(global_fn,
-            with_edge_inputs=with_edge_inputs,
-            with_node_inputs=with_node_inputs,
-            with_global_inputs=with_global_inputs,
-            encoder_size=encoder_size, core_size=core_size,
-             encoder_fn = make_mlp_model,node_encoder_fn = make_multi_mlp_model,edge_encoder_fn = make_heterogeneous_edges_model,
-            name=name, **kwargs)
-
-class GlobalInputLearnerBase(snt.Module):
-
-    def __init__(self,
-        global_fn,
-        with_edge_inputs=False,
-        with_node_inputs=True,
-        with_global_inputs=False,
-        encoder_size: list=None,
-        core_size: list=None,
-        name="GlobalInputLearnerBase",
-        reducer=tf.math.unsorted_segment_sum,
-        encoder_fn = make_mlp_model, **kwargs):
-        super(GlobalInputLearnerBase, self).__init__(name=name)
-
-        if encoder_size is not None:
-            encoder_mlp_fn = partial(encoder_fn, mlp_size=encoder_size, name="EncoderMLP", **kwargs)
-        else:
-            encoder_mlp_fn = partial(ecoder_fn, name='EncoderMLP', **kwargs)
-
-        node_block_args=dict(use_received_edges=False, use_sent_edges=False, use_nodes=True, use_globals=False,received_edges_reducer=reducer,sent_edges_reducer=reducer)
-        edge_block_args=dict(use_edges=False, use_receiver_nodes=True, use_sender_nodes=True, use_globals=False)
-        global_block_args=dict(use_edges=True, use_nodes=True, use_globals=False,nodes_reducer=reducer,edges_reducer=reducer)
-
-        if with_edge_inputs:
-            edge_block_args['use_edges'] = True
-            node_block_args['use_received_edges'] = False
-            node_block_args['use_sent_edges'] = False
-        if not with_node_inputs:
-            edge_block_args['use_receiver_nodes'] = False
-            edge_block_args['use_sender_nodes'] = False
-            node_block_args['use_nodes'] = False
-        if with_global_inputs:
-            global_block_args['use_globals'] = False
-
-        self._edge_encoder_block = blocks.EdgeBlock(
-            edge_model_fn=encoder_mlp_fn,
-            name='edge_encoder_block', **edge_block_args)
-
-        self._node_encoder_block = blocks.NodeBlock(
-            node_model_fn=encoder_mlp_fn,
-            name='node_encoder_block', **node_block_args)
-
-        self._global_block = blocks.GlobalBlock(
-            global_model_fn=encoder_mlp_fn, name='global_encoder_block', **global_block_args)
-        
-        self._global_encoder = blocks.GlobalBlock(
-            global_model_fn=encoder_mlp_fn, name='hlv_encoder_block',use_edges=False,use_nodes=False)
-        
-        if core_size is not None:
-            core_mlp_fn = partial(make_mlp_model, mlp_size=core_size, **kwargs)
-        else:
-            core_mlp_fn = partial(make_mlp_model, **kwargs)
-
-        self._core = MLPGraphNetwork(nn_fn=core_mlp_fn, reducer=reducer)
-
-        self._output_transform = modules.GraphIndependent(None, None, global_fn)
-
-    def __call__(self, input_op, num_processing_steps, is_training=True):
-        node_kwargs = edge_kwargs = global_kwargs = dict(is_training=is_training)
-
-        latent = self._global_block(
-            self._edge_encoder_block(self._node_encoder_block(input_op, node_kwargs), edge_kwargs), global_kwargs)
-        latent0 = latent
-        latent_globals = self._global_encoder(input_op, global_kwargs)
-
-        output_ops = []
-        for _ in range(num_processing_steps):
-            core_input = utils_tf.concat([latent0, latent], axis=1)
-            latent = self._core(core_input, edge_kwargs, node_kwargs, global_kwargs)
-        latent.replace(globals=tf.concat([latent.globals,latent_globals.globals],axis=-1))
-        output_ops.append(self._output_transform(latent))
-
-        return output_ops
-                       
-class GlobalInputClassifier(GlobalInputLearnerBase):
-    def __init__(self,
-        with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
-        encoder_size: list=None, core_size: list=None, decoder_size: list=None,
-        name="GlobalInputClassifier", **kwargs):
-
-        global_output_size = 1
-        if decoder_size is not None:
-            decoder_size += [global_output_size]
-        else:
-            decoder_size = [global_output_size]
-
-        global_fn =lambda: snt.Sequential([
-            snt.nets.MLP(decoder_size,
-                        activation=tf.nn.relu, # default is relu
-                        name='edge_classifier_output'),
-            tf.sigmoid])
-
-        super().__init__(global_fn,
-            with_edge_inputs=with_edge_inputs,
-            with_node_inputs=with_node_inputs,
-            with_global_inputs=with_global_inputs,
-            encoder_size=encoder_size, core_size=core_size,
-            name=name, **kwargs)
-        
         
 class GlobalOnlyLearnerBase(snt.Module):
 
@@ -668,7 +519,7 @@ class GlobalClassifierEdgesFirst(GlobalLearnerBase):
 class HeterogeneousLearnerBase(snt.Module):
     def __init__(self,
         global_fn,
-        with_edge_inputs=False,
+        with_edge_inputs=True,
         with_node_inputs=True,
         with_global_inputs=False,
         encoder_size: list=None,
@@ -691,7 +542,7 @@ class HeterogeneousLearnerBase(snt.Module):
             global_encoder_mlp_fn = partial(global_encoder_fn, name='EncoderMLP')
 
         node_block_args=dict(use_received_edges=False, use_sent_edges=False, use_nodes=True, use_globals=False,received_edges_reducer=reducer,sent_edges_reducer=reducer)
-        edge_block_args=dict(use_edges=False, use_receiver_nodes=True, use_sender_nodes=True, use_globals=False)
+        edge_block_args=dict(use_edges=True, use_receiver_nodes=True, use_sender_nodes=True, use_globals=False)
         global_block_args=dict(use_edges=True, use_nodes=True, use_globals=False,nodes_reducer=reducer,edges_reducer=reducer)
 
         if with_edge_inputs:
@@ -729,9 +580,12 @@ class HeterogeneousLearnerBase(snt.Module):
 
     def __call__(self, input_op, num_processing_steps, is_training=True):
         node_kwargs = edge_kwargs = global_kwargs = dict(is_training=is_training)
+        
+        node_encoding = self._node_encoder_block(input_op, node_kwargs)
+        edge_encoding = self._edge_encoder_block(node_encoding, edge_kwargs)
 
-        latent = self._global_block(
-            self._edge_encoder_block(self._node_encoder_block(input_op, node_kwargs), edge_kwargs), global_kwargs)
+
+        latent = self._global_block(edge_encoding, global_kwargs)
         latent0 = latent
 
         output_ops = []
@@ -744,10 +598,9 @@ class HeterogeneousLearnerBase(snt.Module):
     
 class GlobalClassifierHeterogeneousNodes(HeterogeneousLearnerBase):
     def __init__(self,
-        with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
+        with_edge_inputs=True, with_node_inputs=True, with_global_inputs=False,
         encoder_size: list=None, core_size: list=None, decoder_size: list=None,node_encoder_fn=make_multi_mlp_model,
         name="GlobalClassifierHeterogeneousNodes", **kwargs):
-        #super(GlobalClassifierHeterogeneousNodes, self).__init__()
 
         global_output_size = 1
         if decoder_size is not None:
@@ -770,10 +623,9 @@ class GlobalClassifierHeterogeneousNodes(HeterogeneousLearnerBase):
         
 class GlobalClassifierHeterogeneousEdges(HeterogeneousLearnerBase):
     def __init__(self,
-        with_edge_inputs=False, with_node_inputs=True, with_global_inputs=False,
+        with_edge_inputs=True, with_node_inputs=True, with_global_inputs=False,
         encoder_size: list=None, core_size: list=None, decoder_size: list=None,node_encoder_fn=make_multi_mlp_model,
         name="GlobalClassifierHeterogeneousEdges", **kwargs):
-        #super(GlobalClassifierHeterogeneousEdges, self).__init__()
 
         global_output_size = 1
         if decoder_size is not None:
